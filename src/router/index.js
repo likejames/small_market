27c683072356ea710092ee8a48b5d261e2af37e9
likejames <2319604595@qq.com>
2021-07-09 19:01:41 +0800

    小商场创建

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Commodity from "@/views/Commodity";
+import Test from "@/views/Test"
+import Test1 from "@/views/Test"
 Vue.use(VueRouter)
 //路由配置
-const routes =[]
+const routes = [
+    {
+        path: '/commodity',
+        name: 'Commodity',
+        component: Commodity,
+        children: [{
+            path: '/test',
+            name: 'test',
+            component: Test
+        },
+            {
+                path: '/test1',
+                name: 'test1',
+                component: Test1
+            },]
+    }
+]
 const router = new VueRouter({
-  routes,
-  mode:"history"
+    routes,
+    mode: "history"
 })
 
 export default router
---
+++ src/views/Test1.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>55555</h1>
+</template>
+
+<script>
+export default {
+  name: "Test1"
+}
+</script>
+
+<style scoped>
+
+</style>
