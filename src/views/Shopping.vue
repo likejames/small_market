509e989ac45278058fef4dde52f8471141d61712
likejames <2319604595@qq.com>
2021-07-12 15:33:26 +0800

    商城页面展示卡片优化

--- src/views/Shopping.vue
+++ src/views/Shopping.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="infinite-list-wrapper" @click="isLogin">
+  <div class="infinite-list-wrapper">
     <el-row>
       <el-col :span="4" v-for="(item) in tabledata" :key="item.classId" :offset="1">
         <div style="margin-top:15px">
           <el-card :body-style="{ padding: '0px'}" shadow="hover">
             <img src="../img/img.png"
-                 class="image">
+                 class="image" @click="isLogin">
             <div>
               <span>描述：{{ item.des }} 价格：{{ item.price }}</span><br>
               <div class="bottom clearfix">
@@ -17,6 +17,13 @@
         </div>
       </el-col>
     </el-row>
+    <div>
+      <el-pagination
+          background
+          layout="prev, pager, next"
+          :total="1000">
+      </el-pagination>
+    </div>
   </div>
 </template>
 
@@ -31,6 +38,36 @@ export default {
         des: '这是第一个描述',
         price: 198
       },
+        {
+          src: '../img/img.png',
+          des: '这是第二个描述',
+          price: 198
+        },
+        {
+          src: './img/img.png',
+          des: '这是第三个描述',
+          price: 211
+        },
+        {
+          src: './img/img.png',
+          des: '这是第一个描述',
+          price: 198
+        },
+        {
+          src: './img/img.png',
+          des: '这是第三个描述',
+          price: 211
+        },
+        {
+          src: './img/img.png',
+          des: '这是第一个描述',
+          price: 198
+        },
+        {
+          src: '../img/img.png',
+          des: '这是第一个描述',
+          price: 198
+        },
         {
           src: '../img/img.png',
           des: '这是第二个描述',
